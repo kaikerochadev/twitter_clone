c8d37eaa18e395c8f8fe63dab883808e3b8a9d48
KaikeRochaDev <kaikerochadev@gmail.com>
2023-03-24 19:15:13 -0300

    Displaying tweets from other users

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -34,7 +34,7 @@
         public function getAll(){
             $query = "SELECT t.id, t.id_usuario, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data, u.nome FROM tweets as t
             LEFT JOIN usuarios as u ON t.id_usuario = u.id
-            WHERE id_usuario = :id_usuario ORDER BY t.data DESC";
+            WHERE id_usuario = :id_usuario OR t.id_usuario in (SELECT id_usuario_seguindo FROM usuarios_seguidores WHERE id_usuario = :id_usuario) ORDER BY t.data DESC";
 
             $stmt = $this->db->prepare($query);
             $stmt->bindValue(":id_usuario", $this->__get("id_usuario"));
